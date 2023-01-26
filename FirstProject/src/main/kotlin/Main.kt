cd367199c44010b7e930f3fd88461e7701808601
Emanoel Faria <69714572+Whoefa@users.noreply.github.com>
2023-01-25 22:16:40 -0300

    Add files via upload

---
+++ FirstProject/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main () {
+    var nomeGato:String =""
+    nomeGato="Diego"
+
+    var idadeGato:Int = 0
+    idadeGato=50
+
+    println("O nome do gato é $nomeGato ele tem $idadeGato anos")
+
+}
